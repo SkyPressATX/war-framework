82d56196a1cdeb234b04507d1d41840b670fa463
Ben Moore <bmo@skypress.io>
2018-01-03 19:09:13 -0600

    Starter plugin and theme created

---
+++ wordpress/wp-content/plugins/my-api/my-api.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     My Api
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     my-api
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         My_Api
+ */
