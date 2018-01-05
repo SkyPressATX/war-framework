b600b5fd72e9e357fd782e12c10f7a69a311905a
BMO <bmo@wpengine.com>
2018-01-05 15:03:18 -0600

    shipit.sh fixes

--- shipit.sh
+++ shipit.sh
@@ -3,14 +3,14 @@
 #Version: 0.1.0-alpha
 
 
-if [[ -z ${rep} ]]; then
+if [[ -n ${1} ]]; then
 	dir=$(pwd -P)
 	current_branch=$(git rev-parse --abbrev-ref HEAD)
-	git checkout -b shipit $(git subtree split -P ${rep})
+	git checkout -b shipit $(git subtree split -P wordpress)
 	find ${dir}/wordpress/wp-content -type f -iname "composer.json" -d 3 -execdir composer install --prefer-source -o \;
 	git add . --all
 	git commit -am "Pre-ship commit"
-	git push ${rep} :shipit
+	git push ${1} :shipit
 	git checkout ${current_branch}
 	git branch -D shipit
 	exit 0
