11cc272db84a7f6939a5bc8356592fdb798b2cb0
BMO <bmo@wpengine.com>
2018-01-08 16:05:19 -0600

    WAR Update command

--- war.sh
+++ war.sh
@@ -21,7 +21,7 @@ deploy (){
 			a)
 				ng_build="1";;
 			b)
-				temp_deploy_branch=$OPTARG;;
+				temp_branch=$OPTARG;;
 			B)
 				remote_deploy_branch=$OPTARG;;
 			c)
@@ -39,27 +39,31 @@ deploy (){
 		esac
 	done
 	echo -e "Splitting out WordPress into temporary branch"
-	current_branch=$(git rev-parse --abbrev-ref HEAD)
 	git add . --all && git commit -am "${commit_message}" && git checkout ${deploy_from_branch}
-	git subtree split --prefix=${prefix_path} -b ${temp_deploy_branch} && git checkout ${temp_deploy_branch}
+	git subtree split --prefix=${prefix_path} -b ${temp_branch} && git checkout ${temp_branch}
 	echo -e "Running composer install for plugins and themes"
 	find wp-content -maxdepth 3 -iname "composer.json" -type f -execdir php ${global_composer_path} install --no-dev --prefer-source -o \;
 	find wp-content -iname ".git" -type d -exec rm -rf "{}" \;
-	echo -e "Pushing ${temp_deploy_branch} to ${deploy_remote}:${remote_deploy_branch}"
-	git add . --all && git commit -am "${commit_message}" && git push ${force_push} ${deploy_remote} ${temp_deploy_branch}:${remote_deploy_branch}
+	echo -e "Pushing ${temp_branch} to ${deploy_remote}:${remote_deploy_branch}"
+	git add . --all && git commit -am "${commit_message}" && git push ${force_push} ${deploy_remote} ${temp_branch}:${remote_deploy_branch}
 	echo -e "Cleaning up after ourselves"
-	git checkout ${current_branch} && git branch -D ${temp_deploy_branch}
+	git checkout ${current_branch} && git branch -D ${temp_branch}
 	rm -rf ./wp-content
 	exit 0
 }
 
-upgrade (){ local OPTIND
-
+update (){ local OPTIND
+	git checkout -b ${temp_branch} && git pull ${framework_repo} ${framework_branch} -s recursive -X ours
+	git checkout ${current_branch} && git checkout ${temp_branch} -- ${update_include}
+	git branch -D ${temp_branch}
+	git add . --all && git commit -am 'Post WAR Update'
+	exit 0
 }
 
 #### Process Commands ####
 if [[ -n ${1} ]]; then
 	cmd=${1}
+	current_branch=$(git rev-parse --abbrev-ref HEAD)
 	shift 1
 	declare -F ${cmd} &>/dev/null && ${cmd} $* && exit 0 ||
 		echo -e "\n\033[1;31m"Looks like you entered the wrong function."\033[1;000m\n\nUsage is: war <function> <optional argument>\n"
