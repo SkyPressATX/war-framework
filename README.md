9950606c668f7d100a85e58aaceacc3e47f32743
Ben Moore <bmo-tv@users.noreply.github.com>
2018-01-17 11:47:59 -0600

    v0.4.0-alpha (#2)
    * Updated README.md
    
    * war.sh and war-cli.config files created
    
    * Deploy function added to war-cli
    
    * Readme updated.
    
    * WAR Update command
    
    * WAR Update command
    
    * Pre WAR Update
    
    * Pre WAR Update
    
    * war.sh set created
    
    * remove debugging in war.sh
    
    * remove debugging in war.sh
    
    * minor change to test war.sh update function
    
    * minor change to test war.sh update function
    
    * minor change to test war.sh update function
    
    * war.sh update testing
    
    * war.sh testing
    
    * war.sh testing
    
    * war.sh update tweaks
    
    * war.sh update tweaks
    
    * remove war-cli.config from update_include list
    
    * war.sh and war-cli.config updates
    
    * added fetch step to war.sh update
    
    * v0.3.0-alpha war.sh: Added check-config function
    
    * Remove war-cli.config from version control
    
    * war.sh minor bug fix
    
    * actually we should keep war-cli.config versioned
    
    * v0.3.1-alpha
    
    * Post WAR Update
    
    * Post WAR Update
    
    * war.sh changes
    
    * v0.4.0-alpha War.sh init function built
    
    * remove war-cli.config
    
    * README.md updated
    
    * README.md updated
    
    * README.md updated

--- README.md
+++ README.md
@@ -1,23 +1,112 @@
 # WAR Framework
 This repo holds the build and local development functions for new projects
 
-v0.2.0-alpha
+*v0.4.0-alpha*
 
-Requires:
+## Requires:
 
-* Node
-* NPM
 * Yarn
-* NG-CLI
+* Global @angular/cli
 * Composer
 * Docker
 * Docker Compose
 
-## First steps
+*This document places <example> snippets within it's code. These <examples> are what you will need to changed with your own custom values*
 
-* Clone this repo
-* Run `docker-compose up -d`
-* If Linux: `sudo find wordpress -type d -exec chmod 775 {} \; && sudo find wordpress -type f -exec chmod 664 {} \;`
-* View `README.md` in `wordpress/wp-content/plugins`
-* View `README.md` in `wordpress/wp-content/themes`
-* View `README.md` in `angular`
+## Step 1: Clone Repo
+
+To get you project started with the WAR Framework, clone this repo into a new directory. Once cloned, all other commands should be ran from within `<directory-to-my-app>`
+
+```
+git clone https://github.com/SkyPressATX/war-framework.git <directory-to-my-app>
+cd <directory-to-my-app>
+```
+
+## Step 2: Setup local WordPress with Docker
+
+Included in the WAR Framework is a basic `docker-compose.yml` that will setup WordPress, MySQL, and PHPMyAdmin
+
+```
+docker-compose up -d
+```
+
+We include `-d` to in order to detach this command from your terminal.
+
+Once all containers are up, navigate to `http://localhost:8080` in your web browser. You should see the famous 5 Minute install screen. Install WordPress like normal.
+
+*If you are running Linux, you might need to change the file/folder permissions after your docker containers are setup*
+
+```
+sudo find wordpress -type d -exec chmod 775 {} \; && sudo find wordpress -type f -exec chmod 664 {} \;
+```
+
+## Step 3: Initialize your project with the WAR-cli
+
+The WAR Framework comes with basic CLI to help keep things uniform. This is the `war.sh` script, and it's corresponding `war-cli.config` config file.
+
+```
+./war.sh init -A <my-app-name> -P <angular_prefix> -a
+```
+
+The `init` command will:
+
+* Set proper variables in `war-cli.config`
+* Rename the starter plugin to `wordpress/wp-content/plugins/<my-app-name-api>` and it's main `.php` file to `<my-app-name-api>.php`
+* Run `composer install` in `wordpress/wp-content/plugins/<my-app-name-api>`
+* Rename the starter theme to `wordpress/wp-content/themes/<my-app-name-theme>`
+* Run `composer install` in `wordpress/wp-content/themes/<my-app-name-theme>`
+* Create a new Angular Project in `angular/<my-app-name-theme>` if `-a` flag is used
+* Run `yarn add @skypress/wp-client@latest` in `angular/<my-app-name-theme>`
+* Set proper `outDir` and `angular_prefix` values in `angular/<my-app-name-theme>/.angular-cli.json`
+
+_The `init` command will **not**_
+
+* Edit comment blocks in `wordpress/wp-content/plugins/<my-app-name-api>/<my-app-name-api>.php`
+* Edit comment blocks in `wordpress/wp-content/themes/<my-app-name-theme>/style.css`
+
+## Step 4: Set up remote git repositories
+
+In order to version control you new project, you will need to setup your own remote git repositories. We recommend having at least 2 different repo's:
+
+* A Dev or Pre-Build repo. This is where you can push, pull, and others can contribute to your project and code. This is not your Live or Production version.
+* A Prod or Live repo. This should be a highly restricted repo where few people can push to. This should also be where your built project should be served to the rest of the world (IE: Your hosting provider)
+
+```
+git remote add <dev> <git@github.com:<your-git-user>/<repo-name>.git
+git remote add <prod> <git@<git.myhost.com>:production/<repo-name>.git
+```
+
+## Step 5: Build your App
+
+You are setup! Now you can build out your application. Here are some tips to help you along the way:
+
+* Use `ng generate` in your Angular project (`angular/<my-app-name-theme>`) to quickly build components, modules, etc ...
+* Use `ng serve` to get a fresh version of your pre-build angular app in `http://localhost:4200`
+* Once your template is set, and you are ready to pipe in data from the WordPress REST API, use `ng build --watch`
+ * _Please note, my success with this has been decent at best. You may want to just run `ng build` after each change_
+
+## Step 6: Deploy your App
+
+Alright, everything is looking great and you are ready to deploy live, but you've got a main repo pushing your un-built site to your dev repo. Use the WAR CLI to properly split out the WordPress parts of your project and push them to your remote Production repo
+
+```
+./war.sh deploy -a -r <prod> -c "My commit message"
+```
+
+##### Flags
+
+* `-a` Build your Angular app
+* `-r <prod>` Is the name you gave your remote production repo. See `git remote -v` for the full list of your added repo names
+* `-c "My commit message"` The message you would like to see in your commit
+
+_Want to stop using these flags every time? Have errors about the composer command not being found? Edit the `war-cli.config` file!_
+
+## Step 7: (Optional) Update Project
+
+We've made some updates that you'd like to use, but you don't want to overwrite your changes to this project. A `git pull origin master` would certainly do that. Use the WAR CLI instead:
+
+```
+./war.sh upgrade
+```
+
+This will use `git` to pull in the specific files that aren't relative to your project, instead of the entire framework. Made changes to some files this command updates? Remove them from the `war-cli.config` option `upgrade_include`
---
+++ war.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+#Author: BMO & Antpb
+#Version: 0.4.0-alpha
+
+### Not a command ###
+assign-opts() {
+	local OPTIND
+	while getopts ":aA:b:B:G:D:fm:p:r:" opt; do
+		case $opt in
+			a)
+				angular_build=true;;
+			A)
+				app_slug=$OPTARG;;
+			b)
+				temp_branch=$OPTARG;;
+			B)
+				deploy_to_remote_branch=$OPTARG;;
+			G)
+				global_composer_path=$OPTARG;;
+			D)
+				deploy_from_local_branch=$OPTARG;;
+			f)
+				force_push="-f";;
+			m)
+				commit_message=$OPTARG;;
+			p)
+				prefix_path=$OPTARG;;
+			P)
+				angular_prefix=$OPTARG;;
+			r)
+				deploy_to_remote_repo=$OPTARG;;
+		esac
+	done
+	### Common Variables ###
+	current_branch=$(git rev-parse --abbrev-ref HEAD)
+	br="++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+	epoch=$(date +%s)
+}
+
+#### Functions ####
+## war set <config_key> <config_value>
+set (){
+	sed -Ei '' "s/^(${1}=).*/\1\"${2}\"/" ${config_file}
+}
+
+## war init
+init (){
+	echo -e "${br}Checking Config"
+	rm ${config_file}
+	check-config
+
+	echo -e "${br}Setting up Plugin and Theme"
+	mv ${prefix_path}/wp-content/plugins/my-api/my-api.php ${prefix_path}/wp-content/plugins/my-api/${app_slug}-api.php
+	mv ${prefix_path}/wp-content/plugins/my-api ${prefix_path}/wp-content/plugins/${app_slug}-api
+	mv ${prefix_path}/wp-content/themes/my-theme ${prefix_path}/wp-content/themes/${app_slug}-theme
+
+	echo -e "${br}Installing Composer in plugin/${app_slug}-api"
+	composer install -d=${prefix_path}/wp-content/plugins/${app_slug}-api
+
+	echo -e "${br}Installing Composer in themes/${app_slug}-theme"
+	composer install -d=${prefix_path}/wp-content/themes/${app_slug}-theme
+
+	if [[ true == ${angular_build} ]]; then
+		echo -e "${br}Building Angular project : ${app_slug}-theme"
+		find angular -type f -iname "README.md" -execdir ng new ${app_slug} -is true -it true -p ${angular_prefix} -sg true \;
+		sed -Ei '' "s/^(\"outDir\"\:\s)\"dist\"$/\1\"../../wordpress/wp-content/themes/${app_slug}-theme/src\"/" angular/${app_slug}/.angular-cli.json
+
+		echo -e "${br}Adding WP Client"
+		find angular/${app-slug} -type f -iname "package.json" -execdir yarn add @skypress/wp-client@latest \;
+		find angular/${app-slug} -type f -iname "package.json" -execdir yarn upgrade \;
+	fi
+}
+
+## war deploy -a (Build Angular) -b <temp deploy branch> -B <remote deploy branch> -c <commit message> -C <composer path> -D <deploy from branch> -f (force push) -p <prefix path> -r <remote name>
+deploy (){
+	if [[ -z ${commit_message} ]]; then
+		commit_message="Deploy"
+	fi
+
+	echo -e "${br}Saving current work"
+	git add . --all && git commit -am "${commit_message}"
+	git checkout ${deploy_from_local_branch}
+
+	if [[ ${angular_build} == true ]]; then
+		echo -e "${br}Building Angular Files"
+		find angular -maxdepth 2 -iname "package.json" -type f -execdir yarn build \;
+		echo -e "${br}Commiting build before split"
+		git add . --all && git commit -am "${commit_message}"
+	fi
+
+	echo -e "${br}Splitting out WordPress into temporary branch"
+	git subtree split --prefix=${prefix_path} -b ${temp_branch}
+	git checkout ${temp_branch}
+
+	echo -e "${br}Running composer install for plugins and themes"
+	find wp-content -maxdepth 3 -iname "composer.json" -type f -execdir php ${global_composer_path} install --no-dev --prefer-source -o \;
+	find wp-content -iname ".git" -type d -execdir rm -rf .git \;
+
+	echo -e "${br}Pushing ${temp_branch} to ${deploy_deploy_to_remote_repoemote}:${deploy_to_remote_branch}"
+	git add . --all && git commit -am "${commit_message}"
+	git pull -Xtheirs ${deploy_to_remote_repo} ${deploy_to_remote_branch}
+	git push ${force_push} ${deploy_to_remote_repo} ${temp_branch}:${deploy_to_remote_branch}
+
+	echo -e "${br}Cleaning up after ourselves"
+	git checkout ${current_branch}
+	git branch -D ${temp_branch}
+	rm -rf ./wp-content
+}
+
+## war deploy
+update (){
+	if [[ -z ${commit_message} ]]; then
+		commit_message="Post WAR Update"
+	fi
+
+	echo -e "${br}Fetching from WAR Framework Remote"
+	git fetch ${war_framework_repo} ${war_framework_branch}
+
+	echo -e "${br}Pulling in updated files"
+	git checkout ${war_framework_repo}/${war_framework_branch} -- ${update_include}
+	git add . --all && git commit -am "${commit_message}"
+}
+
+## war check-config
+check-config (){
+	#### CLI Config File ####
+	config_file="./war-cli.config"
+	base_name=$(basename $(pwd))
+	config_vars=( "angular_build" "angular_prefix" "app_slug" "commit_message" "deploy_from_local_branch" "deploy_to_remote_branch" "deploy_to_remote_repo" "global_composer_path" "force_push" "prefix_path" "temp_branch" "update_include" "war_framework_repo" "war_framework_branch" )
+	if [ ! -f ${config_file} ]; then
+		echo "##### WAR Cli Config #####" > ${config_file}
+	fi
+	for v in ${config_vars[*]}; do
+		if [[ -z $(grep "${v}" ${config_file}) ]]; then
+			case ${v} in
+				"angular_build")
+					echo ${v}'=false' >> ${config_file};;
+				"angular_prefix")
+					echo ${v}'="app"' >> ${config_file};;
+				"app_slug")
+					echo ${v}'="'$(basename $(pwd))'"' >> ${config_file};;
+				"commit_message")
+					echo ${v}'=""' >> ${config_file};;
+				"deploy_from_local_branch")
+					echo ${v}'="master"' >> ${config_file};;
+				"deploy_to_remote_branch")
+					echo ${v}'="master"' >> ${config_file};;
+				"deploy_to_remote_repo")
+					echo ${v}'="prod"' >> ${config_file};;
+				"global_composer_path")
+					echo ${v}'="'$(find /usr/local/bin -type f -iname "composer*")'"' >> ${config_file};;
+				"force_push")
+					echo ${v}'=""' >> ${config_file};;
+				"prefix_path")
+					echo ${v}'="wordpress"' >> ${config_file};;
+				"temp_branch")
+					echo ${v}'="temp"' >> ${config_file};;
+				"update_include")
+					echo ${v}'="war.sh docker-compose.yml .gitignore wordpress/.gitignore"' >> ${config_file};;
+				"war_framework_branch")
+					echo ${v}'="master"' >> ${config_file};;
+				"war_framework_repo")
+					echo ${v}'="origin"' >> ${config_file};;
+			esac
+		fi
+	done
+}
+
+#### Process Commands ####
+if [[ -n ${1} ]]; then
+	cmd=${1}
+	shift 1
+	check-config
+	source ${config_file}
+	assign-opts $*
+	declare -F ${cmd} &>/dev/null && ${cmd} && exit 0 ||
+		echo -e "\n\033[1;31m"Looks like you entered the wrong function."\033[1;000m\n\nUsage is: war <function> <optional argument>\n"
+else
+	echo -e "\nUsage is: war <function> <optional argument>\n"
+fi
